eb47ee3ea09d563f211405c0f98406aeb56cab16
qiuming <qiu_ming@hunliji.com>
2017-12-27 15:53:24 +0800

    suport css modules

--- src/Test/index.js
+++ src/Test/index.js
@@ -1,11 +1,11 @@
 import {cube} from '../math'
-import './style.css'
+import style from './style.css'
 import '../moment'
 
 export const say_hello = () => {
     let html = `
-            <h1>这个是标题</h1>
-            <p>这里是内容</p>
+            <h1 class="${style.title}">这个是标题</h1>
+            <p class="${style.text}">这里是内容</p>
     `
     document.getElementById('root').innerHTML = html
     
@@ -15,5 +15,5 @@ export const calc = () => {
         let num = cube(2),
         html = `2*2*2答案等于${num} ,对么,yese？`
         document.getElementById('root').innerHTML = html
-        $('body').css('color','yellow')
+       console.log()
 }
--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -20,7 +20,18 @@ module.exports = {
             },
             {
                 test:/\.css$/,
-                use:['style-loader','css-loader']
+                use:[
+                    'style-loader',
+                    {
+                        loader:'css-loader',
+                        options:{
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: '[name]__[local]___[hash:base64:5]'
+                        }
+                     },
+                    'postcss-loader'
+                    ]
             },
             {
                 test:/\.(png|jpg|svg|gif)$/,
